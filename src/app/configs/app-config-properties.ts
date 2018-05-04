28f0e0b41211a82ae74b2c6453ee0c1adc815627
VAPPDEVELOPER-P\VIBIN <vmangalam@xlbyte.com>
2018-05-04 14:38:21 +0530

    Testing GIT commit.

--- src/app/configs/app-config-properties.ts
+++ src/app/configs/app-config-properties.ts
@@ -2,7 +2,7 @@ export const AppConfigProperties = {
 
   prefix: 'http://cors-anywhere.herokuapp.com/',
   protocol: 'http://',
-  remoteID: '13.126.249.198',
+  remoteID: '13.126.249.100',
   baseURL: '/StreetLight/api/',
   buildBaseURL: function(apiPath) {
     return `${this.prefix}${this.protocol}${this.remoteID}${this.baseURL}${apiPath}`;
