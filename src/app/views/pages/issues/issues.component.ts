f8c223b9091a7d0934e201eb9c6ebf300cda6f9a
VAPPDEVELOPER-P\VIBIN <vmangalam@xlbyte.com>
2018-07-06 19:28:10 +0530

    update issue graph

--- src/app/views/pages/issues/issues.component.ts
+++ src/app/views/pages/issues/issues.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AppUIConfigProperties } from '../../../configs/app-ui-config-properties';
-
+import { IssueService } from '../../../services/issue.service';
 import { ActivitySummaryModel } from '../../../models/activity-summary.model';
+import{Issue} from '../../../models/issue.modal';
+import{GraphService} from '../../../services/graph.service';
 
 @Component({
   selector: 'app-issues',
   templateUrl: './issues.component.html',
   styleUrls: ['./issues.component.less'],
-  providers: [ ActivitySummaryModel ]
+  providers: [ ActivitySummaryModel,IssueService,Issue,GraphService ]
 })
 export class IssuesComponent implements OnInit {
 
@@ -18,6 +20,9 @@ export class IssuesComponent implements OnInit {
   graphData: any = [];
 
   constructor( private changeDetect:ChangeDetectorRef,
+               private _issue:Issue,
+               private _graphService:GraphService,
+               private _issueService:IssueService,
                private _activitySummary: ActivitySummaryModel ) {
     this.appUIConf = AppUIConfigProperties;
     this.activitySummary = _activitySummary;
@@ -28,12 +33,57 @@ export class IssuesComponent implements OnInit {
 
   }
 
-  ngOnInit() {
+    ngOnInit() {
     // Update Graph and summary data
-    this.getIssuesStats();
+      this.getIssuesStats();
+      this.getIssueContent();
+
+    }
+  getIssuesStats() {
+    this._graphService.getGraphData().subscribe(
+    successData => {
+      console.log(successData);
+      this.updateIssuesGraph(successData['IssueStats'])
+    },
+    error => {
+
+    });
+
   }
 
-  getIssuesStats() {
+  getIssueContent() {
+    this._issueService.getIssue().subscribe(
+      successData => {
+         // Success response handler
+          this.getIssueDetails(successData);
+       },
+       error => {
+          // Error response handler
+          this.apiCallFailed(error);
+       }
+    );
+  }
+
+
+  apiCallFailed(error) {
+    console.log(error)
+  }
+
+  getIssueDetails(successData){
+    let issueDataSource=this._issue.getIssue(successData);
+    this.issuesListData.tableData=issueDataSource;
+    this.issuesListData.pageName="issues";
+    this.issuesListData.tableHeaders=this.issueListsHeaders;
+    this.changeDetect.reattach();
+    this.changeDetect.detectChanges();
+  }
+
+
+   /*getIssuesStats() {
+
+
+
+
 
     let successData = {
       TotalIssues: 14,
@@ -44,7 +94,7 @@ export class IssuesComponent implements OnInit {
     setTimeout(() => {this.updateIssuesGraph(successData)}, 1000)
 
 
-    /*this.schedulesService.getDeviceScheduleStats().subscribe(
+   this.schedulesService.getDeviceScheduleStats().subscribe(
     successData => {
         // Success response handler
         this.updateIssuesGraph(successData);
@@ -53,8 +103,8 @@ export class IssuesComponent implements OnInit {
         // Error response handler
         this.apiCallFailed(error);
      }
-    );*/
-  }
+    );
+  }*/
 
   updateIssuesGraph(resData) {
     this.graphData.push(this.activitySummary.getSummaryGraphData(resData, 'issues'));
@@ -72,11 +122,11 @@ export class IssuesComponent implements OnInit {
 
   // Tabular Contents
   issueListsHeaders = [
-    'Iss No.', 'Iss Cat', 'Device ID', 'Created by', 'Assigned to'
+    'Status','Category', 'IssueName','Priority'
   ]
 
   issueLists = [
-    {
+   /* {
       serial: 1,
       category: 'Schedule',
       deviceID: 21,
@@ -117,7 +167,7 @@ export class IssuesComponent implements OnInit {
       deviceID: 17,
       createdBy: 'SreeSrinivasa Rama',
       assignee: 'Mamu Pachakuthira'
-    }
+    }*/
 
   ]
 
